03a2caa36c2d28d5e6736c5625c901775e38071d
Andre Garcia <andrecadgarcia@gmail.com>
2015-02-04 06:16:29 -0600

    Comments and JavaDOC added to GameScene.java and Linha.java

--- src/Scene/GameScene.java
+++ src/Scene/GameScene.java
@@ -1,90 +1,232 @@
 package Scene;
 
-import java.io.IOException;
-
-
 import org.andengine.engine.camera.hud.HUD;
-import org.andengine.entity.IEntity;
 import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.text.Text;
 import org.andengine.entity.text.TextOptions;
 import org.andengine.input.touch.TouchEvent;
-import org.andengine.util.SAXUtils;
 import org.andengine.util.adt.align.HorizontalAlign;
-import org.andengine.util.adt.color.Color;
-import org.andengine.util.level.EntityLoader;
-import org.andengine.util.level.constants.LevelConstants;
-import org.andengine.util.level.simple.SimpleLevelEntityLoaderData;
-import org.andengine.util.level.simple.SimpleLevelLoader;
-import org.xml.sax.Attributes;
-
 import Base.BaseScene;
 import objeto.Linha;
 
 import Manager.SceneManager;
 import Manager.SceneManager.SceneType;
 
+/** Game itself
+*
+* @author Pedro Herig
+* @author Andre Garcia
+* @version 1.0 Feb 4, 2015.
+*/
+
 public class GameScene extends BaseScene implements IOnSceneTouchListener{
 	
-	private static int LevelStartChase = 5;//MAX = 7
-	private static int IndexLinhaAtual = 3;//MAX = 7
+	//-----------------------------------------------
+	//	CONSTANTS
+	//-----------------------------------------------
 	
-	private HUD gameHUD;
-	private Text levelText;
-	private int level = 1;
-	private static Linha linhas[] = new Linha[11];
-	private float frequencia = 1;
-	
-	public static String cor = "";
-	
-	private Background background;
-	
-	private Text gameOverText;
-	private boolean gameOverDisplayed = false;
-	
-	//Leitura do xml
 	private static final String TAG_LINHA = "linha";
 	private static final String TAG_LEVEL = "level";
 	protected static final Object TAG_LINHA_ATTRIBUTE_TYPE_VALUE_VERM = "Verm";
 	protected static final Object TAG_LINHA_ATTRIBUTE_TYPE_VALUE_VERD = "Verd";
 	private static final String TAG_LINHA_ATTRIBUTE_LEVEL = "level";
-
 	private static final int MAX_BLOCOS = 4;
 	protected static final String TAG_LINHA_ATTRIBUTE_TYPE = "color";
 	
-	private void addTolevel(int i)
-	{
-	    level += i;
-	    levelText.setText("" + level);
+	//-----------------------------------------------
+	// STATIC VARIABLES
+	//-----------------------------------------------
+	
+	private static int LevelStartChase = 5; //MAX = 7
+	private static int IndexLinhaAtual = 3; //MAX = 7
+	public static String cor = "";
+	
+	//-----------------------------------------------
+	// VARIABLES
+	//-----------------------------------------------
+	
+	/** HUD */
+	private HUD gameHUD;
+	/** Text of current level */
+	private Text levelText;
+	/**  Current level */
+	private int level;
+	/**  Array of lines shown in user's perspective */
+	private static Linha linhas[];
+	private float frequencia;
+	/** Background */
+	private Background background;
+	/** Text of game over */
+	private Text gameOverText;
+	/** Control if the game is still active */
+	private boolean gameOverDisplayed;
+	
+	//-----------------------------------------------
+	// CONSTRUCTOR
+	//-----------------------------------------------
+	
+	 public GameScene() {
+		this.gameOverDisplayed = false; //Game running 
+		this.frequencia = 1; 
+		this.level = 1; //First level
+	 }
+	 
+	//-----------------------------------------------
+	// LISTENERS
+	//-----------------------------------------------
+	
+	/**Handle touch event
+	 * 
+	 */
+	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+		if (pSceneTouchEvent.isActionDown()) {
+			novaLinhaAtiva(); //Create new line
+	    }
+	    return false;
 	}
 	
-	private void createGameOverText()
-	{
+	/**Handle Back key event
+	 * 
+	 */
+	public void onBackKeyPressed() {
+	    SceneManager.getInstance().loadMenuScene(engine);
+	}
+	
+	
+	//-----------------------------------------------
+	// SCENE CREATION
+	//-----------------------------------------------
+	
+	/**Generate game over text
+	 * 
+	 */
+	private void createGameOverText() {
 	    gameOverText = new Text(0, 0, resourcesManager.font, "Game Over!", vbom);
 	}
-
-	private void displayGameOverText()
-	{
-	    camera.setChaseEntity(null);
-	    gameOverText.setPosition(camera.getCenterX(), camera.getCenterY());
-	    attachChild(gameOverText);
-	    gameOverDisplayed = true;
-	}
 	
-	@Override
-	public void createScene()
-	{
+	/**Create scene to be shown to user 
+	 * 
+	 */
+	public void createScene() {
 	    createBackground();
 	    createHUD();
 	    createGameOverText();
 	    inicializaJogo();
 	    setOnSceneTouchListener(this); 
 	}
+
+	/**Generate the background
+	 * 
+	 */
+	private void createBackground() {
+		background = new Background(238f,233f,233);
+	    setBackground(background);
+	}
 	
-	private void loadLinhaCor(final int level) {
+	/**Create the HUD
+	 * 
+	 */
+	private void createHUD() {
+	    gameHUD = new HUD();
+	    
+	    //Create level text
+	    levelText = new Text(50, 1080 - 150, resourcesManager.font, "Level: 0123456789", new TextOptions(HorizontalAlign.LEFT), vbom);
+	    levelText.setAnchorCenter(0, 0);
+	    //levelText.setColor(Color.WHITE);
+	    levelText.setScale(2f);
+	    levelText.setText("1");
+	    gameHUD.attachChild(levelText);
+	    gameHUD.setZIndex(100);
+	    camera.setHUD(gameHUD);
+	}
+	
+
+	/**Getter of this scene type
+	 * @return SCENE_GAME 
+	 */
+	public SceneType getSceneType() {
+        return SceneType.SCENE_GAME;
+    }
+	
+	/**Description of disposesScene
+	 * 
+	 */
+	public void disposeScene() {
+	    camera.setHUD(null);
+	    camera.setCenter(960, 540);
+	    
+	    camera.setChaseEntity(null);
+
+	    // TODO code responsible for disposing scene
+	    // removing all game scene objects.
+	}
+	
+	/**Description of GameOver()
+	 * 
+	 */
+	private void GameOver() {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	/**Display game over scene
+	 * 
+	 */
+	private void displayGameOverText() {
+	    camera.setChaseEntity(null);
+	    gameOverText.setPosition(camera.getCenterX(), camera.getCenterY());
+	    attachChild(gameOverText);
+	    gameOverDisplayed = true;
+	}
+	
+	/**Generate lines shown to the user
+	 * 
+	 */
+	private void inicializaJogo() {
+		//set the first level
+		this.level = 1;
+		//load color of level
+		loadLinhaCor(this.level);
+		//generate the first line
+		//null means that there is no line under first one
+		linhas[0] = novaLinha(null,MAX_BLOCOS,0, cor);
+		//run first line
+		linhas[0].run();
+		//attach first line to the scene
+		attachChild(linhas[0]);
+		
+		//generate the other lines, basically the same
+		for(int i = 1;i<11;i++){
+			loadLinhaCor(i+1);
+			//number of active blocks is zero in all lines but first 
+			linhas[i] = novaLinha(linhas[i-1],0,100*i,cor);
+			attachChild(linhas[i]);
+		}
+		
+	}
+	
+	//-----------------------------------------------
+	// LINES' TRANSITION LOGIC
+	//-----------------------------------------------
+	/**Increment the level and generate level text
+	 * @param term			number to be added
+	 */
+	private void addTolevel(int term) {
+		//It is added term to current level
+	    this.level += term;
+	    //The new current level is set in text
+	    this.levelText.setText("" + this.level);
+	}
+	
+	/**Load color of a line
+	 * 
+	 * @param level			Level to be loaded			
+	 */
+	private void loadLinhaCor(final int level) {
+
+		//Every 8 lines add bonus line
 		if(level % 9 == 8)
 			cor = "Verd";
 		else
@@ -134,101 +276,50 @@ public class GameScene extends BaseScene implements IOnSceneTouchListener{
 	    */
 	}
 
-	private void inicializaJogo() {
-		level = 1;
-		loadLinhaCor(level);
-		linhas[0] = novaLinha(null,MAX_BLOCOS,0,cor);
-		linhas[0].run();
-		attachChild(linhas[0]);
-		for(int i = 1;i<11;i++){
-			loadLinhaCor(i+1);
-			linhas[i] = novaLinha(linhas[i-1],0,100*i,cor);
-			attachChild(linhas[i]);
-		}
-		
-	}
-
-	private Linha novaLinha(Linha linhaAtual2, int numBlocos1, int y, String color) {
-		// TODO Auto-generated method stub
-		
-		return new Linha(vbom,camera,y,linhaAtual2,numBlocos1,(float)(level)/frequencia, color,resourcesManager.font,vbom);
-	}
-
-	@Override
-    public SceneType getSceneType()
-    {
-        return SceneType.SCENE_GAME;
-    }
-
-	
-	private void createBackground()
-	{
-		background = new Background(238f,233f,233);
-	    setBackground(background);
+	/**Generate a new line
+	 * 
+	 * @param linhaAnterior			Reference to previous line
+	 * @param numBlocos				Number of blocks active in this line
+	 * @param y						Value of line height
+	 * @param color					Color of the line
+	 * @return						New line generated
+	 */
+	private Linha novaLinha(Linha linhaAnterior, int numBlocos, int y, String color) {
+		return new Linha(this.vbom,this.camera,y,linhaAnterior,numBlocos,(float)(this.level)/this.frequencia, color,this.resourcesManager.font,this.vbom);
 	}
 	
-
-	private void createHUD()
-	{
-	    gameHUD = new HUD();
-	    
-	    // CREATE LEVEL TEXT
-	    levelText = new Text(50, 1080 - 150, resourcesManager.font, "Level: 0123456789", new TextOptions(HorizontalAlign.LEFT), vbom);
-	    levelText.setAnchorCenter(0, 0);
-	    //levelText.setColor(Color.WHITE);
-	    levelText.setScale(2f);
-	    levelText.setText("1");
-	    gameHUD.attachChild(levelText);
-	    gameHUD.setZIndex(100);
-	    camera.setHUD(gameHUD);
-	}
-	@Override
-	public void disposeScene()
-	{
-	    camera.setHUD(null);
-	    camera.setCenter(960, 540);
-	    
-	    camera.setChaseEntity(null);
-
-	    // TODO code responsible for disposing scene
-	    // removing all game scene objects.
-	}
-	
-	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent)
-	{
-		if (pSceneTouchEvent.isActionDown())
-	    {
-			novaLinhaAtiva();
-	    }
-	    return false;
-	}
-	
+	/**Description of novaLinhaAtiva()
+	 * 
+	 */
 	private void novaLinhaAtiva() {
-		// TODO Auto-generated method stub
-		if(level <= IndexLinhaAtual){
-			linhas[level - 1].stop();
-			loadLinhaCor(level+1);
-			linhas[level].dispose();
-			linhas[level] = novaLinha(linhas[level - 1],linhas[level - 1].getNumBlocos(),100*(level),cor);
-			attachChild(linhas[level]);
-			linhas[level].run();
+		//stop previous
+		//load color -> dispose -> new line -> Run
+		if(this.level <=  IndexLinhaAtual){
+			linhas[this.level - 1].stop();
+			loadLinhaCor(this.level);
+			linhas[this.level].dispose();
+			linhas[this.level] = novaLinha(linhas[this.level - 1],linhas[this.level - 1].getNumBlocos(),100*(this.level), cor);
+			attachChild(linhas[this.level]);
+			linhas[this.level].run();
 			addTolevel(1);
-		}else{
-			if(linhas[IndexLinhaAtual].stop()){//Se GameOver
+		}else {
+			if(linhas[ IndexLinhaAtual].stop()){//Se GameOver
         		GameOver();	        		
         	}else{//Se ainda tiver blocos
-	        	linhas[IndexLinhaAtual - 1].dispose();
+	        	linhas[ IndexLinhaAtual - 1].dispose();
+	        	//line is assigned to previous line
 	        	for(int i = 1;i < 11;i++){
 	        		linhas[i-1] = linhas[i];
 	        	}
-	        	loadLinhaCor(level+1);
-	        	linhas[IndexLinhaAtual].dispose();
-	        	linhas[IndexLinhaAtual] = novaLinha(linhas[IndexLinhaAtual - 1],linhas[IndexLinhaAtual - 1].getNumBlocos(),100*(level),cor);
-	        	attachChild(linhas[IndexLinhaAtual]);
-	        	loadLinhaCor(level+10-IndexLinhaAtual);
-	        	linhas[10] = novaLinha(linhas[9],0,100*(level+9-IndexLinhaAtual),cor);
+	        	loadLinhaCor(this.level); // IndexLinhaAtual
+	        	linhas[ IndexLinhaAtual].dispose();
+	        	linhas[ IndexLinhaAtual] = novaLinha(linhas[ IndexLinhaAtual - 1],linhas[ IndexLinhaAtual - 1].getNumBlocos(),100*(level),cor);
+	        	attachChild(linhas[ IndexLinhaAtual]);
+	        	loadLinhaCor(this.level+10- IndexLinhaAtual);
+	        	//generate new last line
+	        	linhas[10] = novaLinha(linhas[9],0,100*(this.level+9- IndexLinhaAtual), cor);
 	        	attachChild(linhas[10]);
-	        	linhas[IndexLinhaAtual].run();
+	        	linhas[ IndexLinhaAtual].run();
 	        	addTolevel(1);
         	}
 		}
@@ -257,21 +348,11 @@ public class GameScene extends BaseScene implements IOnSceneTouchListener{
 	        	linhas[1].run();
         	}
 		}*/
-    	if(level >= LevelStartChase){
-    		camera.setCenter(linhas[IndexLinhaAtual+1].getX(), linhas[IndexLinhaAtual+1].getY());
+		
+		//camera starts moving
+    	if(this.level >=  LevelStartChase){
+    		this.camera.setCenter(linhas[ IndexLinhaAtual+1].getX(), linhas[ IndexLinhaAtual+1].getY());
     	}
 	}
-
-	private void GameOver() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onBackKeyPressed()
-	{
-	    SceneManager.getInstance().loadMenuScene(engine);
-	}
-
 
 }
--- src/objeto/Linha.java
+++ src/objeto/Linha.java
@@ -1,66 +1,75 @@
 package objeto;
 
 import org.andengine.engine.camera.Camera;
-import org.andengine.engine.handler.timer.ITimerCallback;
-import org.andengine.engine.handler.timer.TimerHandler;
-import org.andengine.entity.IEntity;
-import org.andengine.entity.modifier.IEntityModifier;
-import org.andengine.entity.modifier.MoveXModifier;
-import org.andengine.entity.modifier.SequenceEntityModifier;
-import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.sprite.TiledSprite;
 import org.andengine.entity.text.Text;
-import org.andengine.extension.physics.box2d.PhysicsConnector;
-import org.andengine.extension.physics.box2d.PhysicsFactory;
-import org.andengine.extension.physics.box2d.PhysicsWorld;
 import org.andengine.opengl.font.Font;
 import org.andengine.opengl.util.GLState;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
-
 import android.opengl.GLES20;
-import android.view.animation.Animation;
-
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-
 import Manager.ResourcesManager;
 
-import  Base.BaseScene;
-
+/** Line object
+*
+* @author Pedro Herig
+* @author Andre Garcia
+* @version 1.0 Feb 4, 2015.
+*/
 public class Linha extends Sprite {
-	private static int Periodo = 7;
+	
+	//-----------------------------------------------
+	//	CONSTANTS
+	//-----------------------------------------------
+	
 	private static final int MAX_BLOCOS = 4;
+	
+	//-----------------------------------------------
+	//	STATIC VARIABLES
+	//-----------------------------------------------
+	
+	private static VertexBufferObjectManager pSpriteVertexBufferObject;
+	private static int Periodo = 7;
+	
+	
+	//-----------------------------------------------
+	//	VARIABLES
+	//-----------------------------------------------
+	
 	private int numBlocos; //Numero de blocos ativos na linha
 	private int posEsq; // Index do bloco mais a esquerdo ativo
 	private boolean sentido; //Tempo por bloco
-	private Linha linhaAnt;
+	private Linha linhaAnterior;
 	private boolean run;
 	private int countUpdate;
 	private String color;
 	private Text texto;
-	private static VertexBufferObjectManager pSpriteVertexBufferObject;
-	
-	private TiledSprite blocos[] = new TiledSprite[12];
+	private TiledSprite blocos[];
 	private float tempo;
 	
-	public Linha(VertexBufferObjectManager pSpriteVertexBufferObject, Camera camera, int y, Linha linha1,int numBlocos1,float tempo, String color,Font fonte, VertexBufferObjectManager vbom)
-	{
+	//-----------------------------------------------
+	//	CONSTRUCTOR
+	//-----------------------------------------------
+
+	public Linha(VertexBufferObjectManager pSpriteVertexBufferObject, Camera camera, int y, Linha linhaAnterior,int numBlocos,float tempo, String color,Font fonte, VertexBufferObjectManager vbom) {
 		super(960, 50+y, 1200, 100, ResourcesManager.getInstance().linha_region, pSpriteVertexBufferObject);
+		this.blocos = new TiledSprite[12];
 		pSpriteVertexBufferObject = vbom;
-		this.linhaAnt = linha1;
+		this.linhaAnterior = linhaAnterior;
 		this.setZIndex(200);
 		this.tempo = tempo;
-		this.numBlocos = numBlocos1;
-		this.posEsq = 1;
-		this.sentido = true;
-		this.setRun(false);
-		this.setCountUpdate(0);
+		this.numBlocos = numBlocos;
+		this.posEsq = 1;		
+		this.sentido = true;	//Right way
+		this.setRun(false);		//It is not running
+		this.setCountUpdate(0); //In the beginning, zero updates
 		this.color = color;
         this.attachBlocos();
-        for(int i = 1;i<1+numBlocos1;i++){
-        	this.blocos[i].setCurrentTileIndex(this.blocos[i].getCurrentTileIndex()-1);
+        //Initializes blocks array
+        for(int i = 1;i < this.numBlocos + 1;i++){
+        	this.blocos[i].setCurrentTileIndex(this.blocos[i].getCurrentTileIndex() - 1);
         }
+        //Set message in the line 
         if(this.color.equals("Verd")){
         	this.texto = new Text(600, 50, fonte, "      + 1 block      ", vbom);
         	this.texto.setZIndex(600);
@@ -73,38 +82,116 @@ public class Linha extends Sprite {
         }
 	}
 	
+	//-----------------------------------------------
+	//	GETTERS
+	//-----------------------------------------------
 	
-	private void acaoLinhaVerd() {
-		// TODO Auto-generated method stub
-		if(this.color.equals("Verd")){
-			if(this.numBlocos == MAX_BLOCOS){
-				this.texto.setText("Max Blocks Reached");
-			}else{
-				TiledSprite plus = null;
-				if(this.posEsq == 1){
-					this.blocos[1+this.numBlocos].setCurrentTileIndex(this.blocos[1 + this.numBlocos].getCurrentTileIndex()-1);
-					plus = new TiledSprite(51+100*(1+this.numBlocos), 51, ResourcesManager.getInstance().plus_region, pSpriteVertexBufferObject);
-				}else{
-					this.posEsq--;
-					plus = new TiledSprite(51+100*(this.posEsq), 51, ResourcesManager.getInstance().plus_region, pSpriteVertexBufferObject);
-					this.blocos[this.posEsq].setCurrentTileIndex(this.blocos[this.posEsq].getCurrentTileIndex()-1);
-				}
-				plus.setCurrentTileIndex(0);
-				plus.setZIndex(500);
-				this.attachChild(plus);
-				this.numBlocos++;
-			}
-		}
-	}
-
-
-	public int getNumBlocos(){
+	/** Getter for number of active blocks in this line
+     * 
+     * @return			Number of active blocks
+     */
+	public int getNumBlocos() {
 		return this.numBlocos;
 	}
 	
-	private void attachBlocos()
-    {
+	/** Getter for number of updates
+	 * 
+	 */
+	public int getCountUpdate() {
+		return countUpdate;
+	}
+	
+	/** Getter for period
+	 * 
+	 * @return			Value of period (used in updates)
+	 */
+	public static int getPeriodo() {
+		return Periodo;
+	}
+
+	/** Getter for line state (Running or not)
+	 * 
+	 * @return			Value of variable run
+	 */
+	public boolean isRun() {
+		return run;
+	}
+	
+	//-----------------------------------------------
+	//	SETTERS
+	//-----------------------------------------------
+	
+	/** Set this line to run
+	 * 
+	 */
+	public void run(){
+		this.setRun(true);
+	}
+	
+	/** Setter for number of updates
+	 * 
+	 * @param countUpdate			Number of updates
+	 */
+	public void setCountUpdate(int countUpdate) {
+		this.countUpdate = countUpdate;
+	}
+
+
+	/** Setter for line state
+	 * 
+	 * @param run			New line state
+	 */
+	public void setRun(boolean run) {
+		this.run = run;
+	}
+	
+	//-----------------------------------------------
+	//	GAME
+	//-----------------------------------------------
+	
+	/** Controls the game execution
+	 * 
+	 * @return			When true, the game stops
+	 */
+	public Boolean stop() {
+		this.setRun(false);
+		if(this.linhaAnterior != null) { //when it is not the very first line
+			for(int i = 0;i< 12;i++){
+				if(this.blocos[i].getCurrentTileIndex() < this.linhaAnterior.blocos[i].getCurrentTileIndex()){
+					this.retiraBloco(i); //if a block has no block under itself, turn this block inactive
+				}
+			}
+			if(this.numBlocos == 0) 
+				return true; //if the number of active blocks reaches zero, the game stops
+			this.acaoLinhaVerd();
+		}
+		return false; //There are still active blocks in the game, keep playing
+	}
+	
+	/** Controls the update frequency of the game
+	 *  
+	 */
+	protected void draw(final GLState pGLState, final Camera pCamera) {
+		this.mSpriteVertexBufferObject.draw(GLES20.GL_TRIANGLE_STRIP, Sprite.VERTICES_PER_SPRITE);
+		if(this.isRun()){ //Game loop
+        	this.countUpdate++; 
+        	if(this.countUpdate % Linha.Periodo == Linha.Periodo - 1) { 
+        		this.passo();	//Update
+        		this.countUpdate = 0; //reset update variable
+	        }
+        }
+	}
+	
+	//-----------------------------------------------
+	//	BLOCKS' CONTROL LOGIC
+	//-----------------------------------------------
+	
+	/** Attaches the right block color to the lines, also dim boundaries blocks
+	 * 
+	 */
+	private void attachBlocos() {
         for(int i = 0;i< 12;i++){
+        	//choose among blue, red and green blocks to be set
         	if(this.color == "Azul")
         		this.blocos[i] = new TiledSprite(51+100*i, 51, ResourcesManager.getInstance().bloco_region_azul, pSpriteVertexBufferObject);
         	else if(this.color == "Verm")
@@ -113,6 +200,7 @@ public class Linha extends Sprite {
         		this.blocos[i] = new TiledSprite(51+100*i, 51, ResourcesManager.getInstance().bloco_region_verd, pSpriteVertexBufferObject);
         	this.attachChild(this.blocos[i]);
         	if(i == 0 || i == 11){
+        		//if block is in the boundaries, dim the block
         		this.blocos[i].setCurrentTileIndex(2);
         	}else{
         		this.blocos[i].setCurrentTileIndex(1);
@@ -120,18 +208,63 @@ public class Linha extends Sprite {
         }
     }
 	
+	/** Add one more block to the active blocks, if the number of blocks is not the maximum already
+	 * 
+	 */
+	private void acaoLinhaVerd() {
+		if(this.color.equals("Verd")){
+			if(this.numBlocos == MAX_BLOCOS){ //No block to be added
+				this.texto.setText("Max Blocks Reached");
+			}else{
+				TiledSprite plus = null;
+				if(this.posEsq == 1){
+					//if the most left block is in the first position, add next to the most right block 
+					this.blocos[1+this.numBlocos].setCurrentTileIndex(this.blocos[1 + this.numBlocos].getCurrentTileIndex()-1);
+					plus = new TiledSprite(51+100*(1+this.numBlocos), 51, ResourcesManager.getInstance().plus_region, pSpriteVertexBufferObject);
+				}else{
+					//add one block to the left of the most left block
+					this.posEsq--;
+					plus = new TiledSprite(51+100*(this.posEsq), 51, ResourcesManager.getInstance().plus_region, pSpriteVertexBufferObject);
+					this.blocos[this.posEsq].setCurrentTileIndex(this.blocos[this.posEsq].getCurrentTileIndex()-1);
+				}
+				plus.setCurrentTileIndex(0);
+				plus.setZIndex(500);
+				this.attachChild(plus);
+				this.numBlocos++; //Increment number of blocks by 1
+			}
+		}
+	}
 	
+	/**
+	 * 
+	 * @param index			Position in the blocks array needed to be deactivated
+	 */
+	private void retiraBloco(int index) {
+		if(this.posEsq == index) 
+			posEsq++; //turn the second block the most left block
+		this.blocos[index].setCurrentTileIndex(
+				this.blocos[index].getCurrentTileIndex() + 1);
+		this.numBlocos--; //Decrease number of active blocks by 1 
+		return;
+	}
+
+	//-----------------------------------------------
+	//	BLOCKS' MOVEMENT LOGIC
+	//-----------------------------------------------
 	
-	public void passo()
-	{
+	/** Controls movement of the active blocks (Player)
+	 * 
+	 */
+	public void passo() {
 		
+		//when reach the boundaries, directions is changed
 		if(this.posEsq == 0 || this.posEsq + this.numBlocos == 12){
 			if(sentido)
 				sentido = false;
 			else
 				sentido = true;
 		}
-		if(sentido){
+		if(sentido){ //moving to the right
 			//Apaga bloco
 			this.blocos[this.posEsq].setCurrentTileIndex(
 					this.blocos[this.posEsq].getCurrentTileIndex() + 1);
@@ -140,7 +273,7 @@ public class Linha extends Sprite {
 					this.blocos[this.posEsq+this.numBlocos].getCurrentTileIndex() - 1);
 			posEsq++;
 			
-		}else{
+		}else{	//moving to the left
 			posEsq--;
 			//Acende bloco
 			this.blocos[this.posEsq].setCurrentTileIndex(
@@ -151,69 +284,5 @@ public class Linha extends Sprite {
 			
 		}
 		
-	}
-	
-	private void retiraBloco(int i){
-		if(this.posEsq == i)
-			posEsq++;
-		this.blocos[i].setCurrentTileIndex(
-				this.blocos[i].getCurrentTileIndex() + 1);
-		this.numBlocos--;
-		return;
-	}
-	
-	public Boolean stop(){ //Retorna GameOver(True) ou nao
-		this.setRun(false);
-		if(this.linhaAnt != null){
-			for(int i = 0;i< 12;i++){
-				if(this.blocos[i].getCurrentTileIndex() < this.linhaAnt.blocos[i].getCurrentTileIndex()){
-					this.retiraBloco(i);
-				}
-			}
-			if(this.numBlocos == 0)
-				return true;
-			this.acaoLinhaVerd();
-		}
-		return false;
-	}
-	
-	public void run(){
-		this.setRun(true);
-	}
-
-	@Override
-	protected void draw(final GLState pGLState, final Camera pCamera) {
-		this.mSpriteVertexBufferObject.draw(GLES20.GL_TRIANGLE_STRIP, Sprite.VERTICES_PER_SPRITE);
-		if(this.isRun()){
-        	this.countUpdate++;
-        	if(this.countUpdate % Linha.Periodo == Linha.Periodo - 1){
-        		this.passo();
-        		this.countUpdate = 0;
-	        }
-        }
-	}
-
-	public int getCountUpdate() {
-		return countUpdate;
-	}
-
-
-	public void setCountUpdate(int countUpdate) {
-		this.countUpdate = countUpdate;
-	}
-
-
-	public static int getPeriodo() {
-		return Periodo;
-	}
-
-
-	public boolean isRun() {
-		return run;
-	}
-
-
-	public void setRun(boolean run) {
-		this.run = run;
 	}
 }
